afc2e4936db91bea89c61f7470f5b3cb0f35655c
Arima057nn <tjendung97pain@gmail.com>
2023-04-12 16:04:03 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { Stack } from "@mui/material";
 import Todo from "./Todo";
+import { useTheme } from "@mui/material/styles";
+import BgImage from "./assets/learning.jpg";
 
 function App() {
+  const theme = useTheme();
   return (
-    <Stack direction="row" justifyContent="center">
+    <Stack
+      direction="row"
+      justifyContent="center"
+      sx={{
+        backgroundImage: `url(${BgImage})`,
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
       <Todo />
     </Stack>
   );
--- src/Todo/index.jsx
+++ src/Todo/index.jsx
@@ -1,7 +1,15 @@
 import styles from "./Todo.module.scss";
 import classNames from "classnames/bind";
-import { Box, Button, Checkbox, Stack, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const cx = classNames.bind(styles);
 
@@ -17,14 +25,31 @@ function Todo() {
   };
 
   const handleCompleted = (index) => {
-    const updatedTodos = [...todos]; // Tạo bản sao của mảng todos
+    const updatedTodos = [...todos]; // Tạo bản sao của mảng todos bằng cách sử dụng toán tử spread
     updatedTodos[index].completed = !updatedTodos[index].completed; // Đảo ngược trạng thái hoàn thành của công việc có chỉ số index
     setTodos(updatedTodos); // Cập nhật lại mảng todos với công việc đã được đánh dấu hoàn thành
   };
+  const handleRemove = (index) => {
+    const removedArr = [...todos].filter((todo, i) => index !== i);
+    setTodos(removedArr); // Cập nhật lại mảng todos với công việc đã được đánh dấu hoàn thành
+  };
 
   return (
-    <Box width={560} backgroundColor={"#ebbd94"}>
-      <h3 className={cx("red")}>Todo-list</h3>
+    <Box
+      width={600}
+      height={760}
+      backgroundColor={"#f6f9fc"}
+      borderRadius={4}
+      mt={4}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <h1 className={cx("title")}>Todo-list</h1>
+      </div>
       <Stack
         spacing={4}
         direction="row"
@@ -47,34 +72,48 @@ function Todo() {
         </Button>
       </Stack>
 
-      <Stack direction="column">
-        <ul className={cx("ulli")}>
+      <Box sx={{ width: "100%" }}>
+        <ul className={cx("list")}>
           {todos.map((job, index) => {
             return (
-              <Stack key={index} direction="column">
-                <li
-                  className={cx({
-                    red: job.completed, // Nếu công việc đã được đánh dấu hoàn thành (completed = true) thì sử dụng lớp CSS "red"
-                    blue: !job.completed, // Ngược lại, sử dụng lớp CSS "blue"
-                  })}
-                >
-                  <Stack
-                    direction="row"
-                    alignItems="center"
-                    sx={{ width: 500 }}
-                  >
+              <li
+                style={{ display: "flex", justifyContent: "space-between" }}
+                key={index}
+                className={cx({
+                  red: job.completed, // Nếu công việc đã được đánh dấu hoàn thành (completed = true) thì sử dụng lớp CSS "red"
+                  blue: !job.completed, // Ngược lại, sử dụng lớp CSS "blue"
+                })}
+              >
+                <div>
+                  {job.completed ? (
+                    <Checkbox disabled checked />
+                  ) : (
                     <Checkbox
                       checked={job.completed} // Đặt trạng thái hoàn thành của Checkbox bằng với trạng thái của công việc
                       onChange={() => handleCompleted(index)} // Khi Checkbox được thay đổi trạng thái, gọi hàm handleCompleted để cập nhật trạng thái hoàn thành của công việc tương ứng
                     />
-                    {job.title}
-                  </Stack>
-                </li>
-              </Stack>
+                  )}
+                  {job.title}
+                </div>
+                <div>
+                  {job.completed ? (
+                    <IconButton aria-label="delete" disabled>
+                      <DeleteIcon />
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => handleRemove(index)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  )}
+                </div>
+              </li>
             );
           })}
         </ul>
-      </Stack>
+      </Box>
     </Box>
   );
 }
